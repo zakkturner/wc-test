5e0efe983aae99dfdbabacd09d3f0eebc9432e11
zakkturner <zakkturner@yahoo.com>
2023-06-19 20:29:03 -0400

    started feature and benefits on mobile

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 
 const header = document.querySelector('.header');
-
+const tab1 = document.querySelector('tab-1');
+const tab2 = document.querySelector('tab-2');
+const tab3 = document.querySelector('tab-3');
+const tabs = document.querySelectorAll('features-accordion-tabs')
+tabs.forEach(tab => {
+    tab.addEventListener('click', () =>{
+        alert('clicked')
+        console.log(tab)
+    })
+});
 
 
 // Scroll Animation
